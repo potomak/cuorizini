4d435813f7bf231cfb52da37a06410e0c77d9dd2
Giovanni <potomak84@gmail.com>
2010-10-22 12:42:29 +0200

    initial import

---
+++ public/play_cuorizini.js
@@ -0,0 +1,8 @@
+// soundManager.onready(function() {
+//   if (soundManager.supported()) {
+//     // SM2 is ready to go!
+//     makeSomeNoise(); // soundManager.createSound(), etc.
+//   } else {
+//     // unsupported/error case
+//   }
+// });
